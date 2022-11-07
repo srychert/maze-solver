51764f261fbde260cfeb9b5ffb50891de5d9770e
SebastianRychert <sebastian_rychert@wp.pl>
2022-11-08 00:05:44 +0100

    remove compiled js files

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const BFS_1 = __importDefault(require("./BFS"));
-const bfs = new BFS_1.default([['S', '.', 'E']]);
-console.log(bfs.serach());
-//# sourceMappingURL=index.js.map
--- dist/maze.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isTile = exports.ALL_TILES = void 0;
-exports.ALL_TILES = ['S', 'E', '#', '.'];
-function isTile(value) {
-    return exports.ALL_TILES.includes(value);
-}
-exports.isTile = isTile;
-//# sourceMappingURL=maze.js.map
